aee3ecd39685ae76d68a86c1da2f23394379e517
Pavel <Chesh397@mail.ru>
2019-06-09 16:20:24 +0300

    Fix bug with inconsist history after move changing

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -415,9 +415,8 @@ app.intent(
     const correctCtx = conv.contexts.get('correct-move');
     if (correctCtx) {
       const board = new ChessBoard(fenstring);
-      const histLength = conv.user.storage.history.length;
-      const lastAIMove = conv.user.storage.history[histLength - 1];
-      const lastPlayerMove = conv.user.storage.history[histLength - 2];
+      const lastAIMove = conv.user.storage.history.pop();
+      const lastPlayerMove = conv.user.storage.history.pop();
       board.extract(lastAIMove.m, lastAIMove.b);
       board.extract(lastPlayerMove.m, lastPlayerMove.b);
       fenstring = board.convertToFen();
