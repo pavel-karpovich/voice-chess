2d013afc0bd8dfca95965884443c8d453ccc5cea
Pavel Karpovich <chesh397@mail.ru>
2019-07-18 17:25:10 +0300

    Add warning log to catch MaxListenersExceeded

--- functions/src/firebase.ts
+++ functions/src/firebase.ts
@@ -2,3 +2,4 @@ import * as functions from 'firebase-functions';
 import { app } from './app';
 
 export const fulfillment = functions.https.onRequest(app);
+process.on('warning', e => console.warn(e.stack));
